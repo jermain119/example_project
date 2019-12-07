ce3a78f1edd7ef4cc67b0faf2e00c843e37176cc
joshua richards <jermain119@gmail.com>
2019-12-07 19:29:47 +0000

    fourth commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+  class UsersController < ApplicationController
+
+  #login
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/books'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:email => params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/books'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  #logout
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+  #signup
+  get '/signup' do
+    if !logged_in?
+      erb :'users/signup'
+    else
+      redirect to '/books'
+    end
+  end
+
+  post '/signup' do
+    if params[:name] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/books'
+    end
+  end
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  validates_presence_of :name, :email, :password
+  has_secure_password
+  has_many :books
+end
